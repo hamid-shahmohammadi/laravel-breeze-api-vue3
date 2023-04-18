d0a767b0276a80f91b6268c0f85d8eb043867b30
Hamid Shah Mohammadi <43700170+hamid-shahmohammadi@users.noreply.github.com>
2023-04-18 10:55:39 +0430

    02-01-29-1055

--- vue3/src/components/Register.vue
+++ vue3/src/components/Register.vue
@@ -1,11 +1,60 @@
-<script setup lang="ts">
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+import { useRouter } from 'vue-router';
 
+const router = useRouter()
+const form=ref({
+    name:'',
+    email:'',
+    password:'',
+    password_confirmation:''
+})
+
+const handleRegister = async () => {
+    await axios.post('/register',{
+        name:form.value.name,
+        email:form.value.email,
+        password:form.value.password,
+        password_confirmation:form.value.password_confirmation
+    })
+    router.push('/')
+}
 </script>
 
 <template>
-<h2>Register</h2>
-</template>
+    <div class="bg-gray-300 rounded-md p-4 mt-24 mx-auto w-1/2">
+        <form @submit.prevent="handleRegister">
+            <div class="mb-6">
+                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Name</label>
+                <input v-model="form.name" type="text"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required>
+            </div>
+            <div class="mb-6">
+                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                <input v-model="form.email" type="email" id="email"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required>
+            </div>
+            <div class="mb-6">
+                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
+                    password</label>
+                <input v-model="form.password" type="password"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required>
+            </div>
+            <div class="mb-6">
+                <label for="password_confirmation" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
+                    password confirmation</label>
+                <input v-model="form.password_confirmation" type="password"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required>
+            </div>
 
-<style scoped>
+            <button type="submit"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+        </form>
+</div></template>
 
-</style>
+<style scoped></style>
